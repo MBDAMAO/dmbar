9d3812575a896d870c358934fe122ebf279e4439
damao <3034906016@qq.com>
2024-12-08 22:08:05 +0800

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,7 @@ mod tray;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    // 单例检测
+    // check singleton
     // let app_exists: bool = tauri::async_runtime::block_on(async move {
     //     if server::check_singleton().await.is_err() {
     //         println!("app exists");
@@ -18,7 +18,13 @@ pub fn run() {
     //     return;
     // }
 
-    tauri::Builder::default()
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
+    #[cfg(debug_assertions)]
+    let devtools = tauri_plugin_devtools::init();
+
+    let mut builder = tauri::Builder::default()
         .setup(|app| {
             #[cfg(all(desktop))]
             {
@@ -28,13 +34,20 @@ pub fn run() {
             Ok(())
         })
         .setup(init::init)
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             cmds::greet,
             cmds::get_kline,
             cmds::get_orders,
             api::get_request
-        ])
-        .run(tauri::generate_context!())
+        ]);
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+    let app = builder
+        .build(tauri::generate_context!())
         .expect("error while running tauri application");
+    app.run(|_, _| {});
 }
