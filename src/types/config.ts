0e56dbd34e7a9a7f792b7c47d792e0eb13fc5d04
Damao <3034906016@qq.com>
2024-12-29 23:46:22 +0800

    默认配置

--- src/types/config.ts
+++ src/types/config.ts
@@ -39,17 +39,17 @@ export const defaultConfig: Config = {
           sub: "Security, Cloud, and Subscription",
           image:
             "https://gravatar.com/avatar/f4fd76a3084fa1ad216c7906d14b0e64?size=256",
-          value: "Account",
+          value: "Account12",
           options: [
             {
               name: "Account",
               sub: "Security, Cloud, and Subscription",
-              value: "Account",
+              value: "Account1",
             },
             {
               name: "Account",
               sub: "Security, Cloud, and Subscription",
-              value: "Account",
+              value: "Account12",
             },
           ],
         },
@@ -60,32 +60,18 @@ export const defaultConfig: Config = {
       title: "General Settings",
       components: [
         {
-          type: "switch",
-          name: "Enable Notifications",
-          sub: "Notification settings for your account",
-          value: false,
-        },
-        {
           type: "selector",
           name: "Theme",
           sub: "Dark or light theme",
           value: "dark",
           options: [
             {
-              name: "dark",
-              sub: "Security, Cloud, and Subscription",
-              value: "Account",
+              name: "transprant",
+              sub: "transprant background",
+              value: "transprant",
             },
-            {
-              name: "light",
-              sub: "Security, Cloud, and Subscription",
-              value: "Account",
-            },
-            {
-              name: "system",
-              sub: "Security, Cloud, and Subscription",
-              value: "Account",
-            },
+            { name: "light", sub: "ligth theme", value: "light" },
+            { name: "dark", sub: "dark theme", value: "dark" },
           ],
         },
       ],
@@ -97,12 +83,8 @@ export const defaultConfig: Config = {
         {
           type: "input",
           placeholder: "Enter your preferred video quality...",
-        },
-        {
-          type: "switch",
-          name: "Enable HD",
-          sub: "HD video quality settings",
-          value: true,
+          cookie:
+            "buvid3=063B581A-C6F5-EE0B-C43F-830C290D5BB209485infoc; b_nut=1714128009; _uuid=261093CFF-7F96-69CC-99AD-C2D1F421B83B06421infoc; enable_web_push=DISABLE; FEED_LIVE_VERSION=V_WATCHLATER_PIP_WINDOW3; buvid4=AACD56BA-1BAC-AF79-AF92-2A6F4D17EA1C35455-022112711-s62au2mc03Xvrbf7mUgygA%3D%3D; rpdid=|(umR|Y|k~Ru0J'u~uRuk)u|l; buvid_fp_plain=undefined; DedeUserID=330838998; DedeUserID__ckMd5=881a8a520eb829f4; header_theme_version=CLOSE; hit-dyn-v2=1; LIVE_BUVID=AUTO1017155301152291; CURRENT_QUALITY=80; fingerprint=4e109b97323e7386f22aedddcb16f57f; PVID=1; is-2022-channel=1; buvid_fp=4e109b97323e7386f22aedddcb16f57f; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzU2MzcyNjgsImlhdCI6MTczNTM3ODAwOCwicGx0IjotMX0.82uQlpOELZVp3S2gzt0CdHy6i_hUk-3UMHQfChzxkYA; bili_ticket_expires=1735637208; SESSDATA=b5d096c5%2C1751001192%2Cfd64d%2Ac2CjBtoPYDkFvkKMasb0Y-a5NxG9Gkc6_X9BKMDZaM3FfD9OQme7qeetCqmzKZLKXLxf8SVmt0V2tKaW9aUS1mcUtGYVhDUXpIaG8tVWdhR2xwRGw5QTJyQzdEdmlyMEdPbUtHR2RPelZHQnRGRnF4ejZsOVFRa3lWZ2sycE5zWFBpUFZQWXhmQzJRIIEC; bili_jct=9c42a0041d45631ed0b8eff4ad0657c7; CURRENT_FNVAL=4048; b_lsid=B4FEA442_19413132D44; home_feed_column=4; browser_resolution=927-898; bp_t_offset_330838998=1016397220255105024",
         },
       ],
     },
